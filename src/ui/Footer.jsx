3eca0f67c6d34dd25ce33cf3960015e05368eeb8
Vladyslav Zyma <vladzima13@gmail.com>
2023-10-25 22:24:11 +0300

    add styles

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,7 +1,11 @@
 import styles from "./Footer.module.css";
 
 function Footer() {
-  return <footer className={styles.footer}>FOOTER</footer>;
+  return (
+    <footer className={styles.footer}>
+      <h2>And here a very beautiful footer 😉</h2>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,7 +1,11 @@
 import styles from "./Header.module.css";
 
 function Header() {
-  return <header className={styles.header}>HEADER</header>;
+  return (
+    <header className={styles.header}>
+      <h2>Just imagine that you see here a very beautiful header 😉</h2>
+    </header>
+  );
 }
 
 export default Header;
